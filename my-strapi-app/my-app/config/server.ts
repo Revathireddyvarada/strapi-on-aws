cc59f6bc4c632724bf9e519154ffbca704578eca
Ubuntu <ubuntu@ip-172-31-6-6.ap-south-1.compute.internal>
2025-08-05 12:41:48 +0000

    changes happened

--- my-strapi-app/my-app/config/server.ts
+++ my-strapi-app/my-app/config/server.ts
@@ -1,7 +1,15 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://13.233.0.42:1337'), // <- Important for admin login
   app: {
     keys: env('APP_KEYS', '').split(','),
   },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    url: '/admin',
+  },
 });
+
